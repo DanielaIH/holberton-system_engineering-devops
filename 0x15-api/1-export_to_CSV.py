faefd35cb3956f89da1755cd1cff9de5a8330131
DanielaIH <4427@holbertonschool.com>
2022-09-20 20:39:59 -0500

    ADD 1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Python script to export data in the CSV format."""
+
+import requests
+from sys import argv
+import csv
+
+if __name__ == "__main__":
+    tasks = []
+
+    id_e = int(argv[1])
+    api_url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get("{}users/{}".format(api_url, id_e)).json()
+    todos = requests.get("{}users/{}/todos".format(api_url, id_e)).json()
+
+    filepath = "{}.csv".format(id_e)
+    with open(filepath, "w") as file:
+        writer = csv.writer(file, delimiter=",", quoting=csv.QUOTE_ALL)
+
+        for x in todos:
+            writer.writerow([id_e, user["username"], x["completed"],
+                            x["title"]])
+    file.close()
